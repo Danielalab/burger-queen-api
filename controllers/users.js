9da0d20f2d5dafbba24ef99bcb902d3a0cb4c919
Danielalab <olabolacrayola@hotmail.com>
2019-07-29 13:46:42 -0500

    adding /users POST

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,34 @@
+const db = require('../mongodb');
+const bcrypt = require('bcrypt');
+
 const getUsers = (req, resp) => {};
 
 const getUserById = (req, resp) => {};
 
-const addUser = (req, resp, next) => {};
+const addUser = async (req, resp, next) => {
+  const { email, password, roles } = req.body;
+  if (!email || !password) {
+    next(400);
+  }
+  try {
+    const collectionUsers = (await db()).collection('users');
+    const userExist = await collectionUsers.findOne({ email });
+    if (userExist) {
+      return next(403);
+    }
+    await collectionUsers.insertOne({
+      email,
+      password: bcrypt.hashSync(password, 10),
+      roles: roles || { admin: false }
+    });
+    const users = (await collectionUsers.find().toArray())
+      .map(({ _id, email, roles }) => ({ _id, email, roles }));
+    resp.send(users);
+  } catch (error) {
+    next(500);
+  }
+
+};
 
 const deletedUser = (req, resp, next) => {};
 
