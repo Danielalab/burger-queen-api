5d235727bbeecb20494557a30257e9aef16ecaec
Danielalab <olabolacrayola@hotmail.com>
2019-11-07 18:14:59 -0500

    agregando unit testing deleteUser

--- controllers/__tests__/users.spec.js
+++ controllers/__tests__/users.spec.js
@@ -91,3 +91,76 @@ describe('addUsers', () => {
     addUser(req, {}, next);
   })
 })
+
+describe('deleteUser', () => {
+  let users = null;
+  beforeAll(async () => {
+    await db();
+    const collectionUsers = (await db()).collection('users');
+    users = await collectionUsers.insertMany([
+      {
+        email: 'test@test',
+        roles: { admin: false },
+      },
+      {
+        email: 'test@delete',
+        roles: { admin: false },
+      }
+    ]);
+  });
+
+  afterAll(async () => {
+    await db().close();
+  });
+
+  it('Deberia de poder eliminar un usuario por su uid', (done) => {
+    const userId = users.insertedIds['0'];
+    const req = {
+      params: {
+        uid: userId,
+      }
+    }
+
+    const resp = {
+      send: (response) => {
+        expect(response._id).toEqual(userId);
+        done();
+      }
+    }
+
+    deleteUser(req, resp);
+  })
+
+  it('Deberia de poder eliminar un usuario por su email', (done) => {
+    const req = {
+      params: {
+        uid: 'test@delete',
+      }
+    }
+
+    const resp = {
+      send: (response) => {
+        expect(response.email).toBe('test@delete');
+        done();
+      }
+    }
+
+    deleteUser(req, resp);
+  })
+
+  it('Deberia de mostrar un error 404 si el usuario no existe', (done) => {
+    const req = {
+      params: {
+        uid: 'abc123',
+      }
+    }
+
+    const next = (code) => {
+        expect(code).toBe(404);
+        done();
+      }
+
+    deleteUser(req, {}, next);
+  })
+
+})
