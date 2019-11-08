175d1c9528720d9064a9bbd048d88eebdfbd3102
Danielalab <olabolacrayola@hotmail.com>
2019-11-08 18:40:23 -0500

    agregando unit testing updateUser

--- controllers/__tests__/users.spec.js
+++ controllers/__tests__/users.spec.js
@@ -162,5 +162,205 @@ describe('deleteUser', () => {
 
     deleteUser(req, {}, next);
   })
+})
 
+describe('updateUser', () => {
+  let users = null;
+  beforeAll(async () => {
+    await db();
+    const collectionUsers = (await db()).collection('users');
+    users = await collectionUsers.insertMany([
+      {
+        email: 'test@test',
+        roles: { admin: true },
+      },
+      {
+        email: 'test2@test',
+        roles: { admin: false },
+      }
+    ]);
+  });
+
+  afterAll(async () => {
+    await db().close();
+  });
+
+  it('Deberia de poder actualizar un usuario por su uid', (done) => {
+    const userId = users.insertedIds['0'];
+    const req = {
+      params: {
+        uid: userId,
+      },
+      headers: {
+        authenticatedUser: {
+          roles: {
+            admin: true,
+          }
+        }
+      },
+      body: {
+        email: 'email@update'
+      }
+    }
+
+    const resp = {
+      send: (response) => {
+        expect(response._id).toEqual(userId);
+        done();
+      }
+    }
+
+    updateUser(req, resp);
+  })
+
+  it('Deberia de poder actualizar un usuario por su email', (done) => {
+    const req = {
+      params: {
+        uid: 'test2@test',
+      },
+      headers: {
+        authenticatedUser: {
+          roles: {
+            admin: true,
+          }
+        }
+      },
+      body: {
+        email: 'email2@update'
+      }
+    }
+
+    const resp = {
+      send: (response) => {
+        expect(response.email).toBe('test2@test');
+        done();
+      }
+    }
+
+    updateUser(req, resp);
+  })
+
+  it('Deberia de mostrar un error 404 si el usuario no existe', (done) => {
+    const req = {
+      params: {
+        uid: 'abc123',
+      },
+      body: {
+        email: 'test@notuser'
+      }
+    }
+
+    const next = (code) => {
+        expect(code).toBe(404);
+        done();
+      }
+
+    updateUser(req, {}, next);
+  })
+
+  it('Deberia de mostrar un error 403 si un usuario no admin intenta modificar su role', (done) => {
+    const userId = users.insertedIds['0'];
+    const req = {
+      params: {
+        uid: userId,
+      },
+      headers: {
+        authenticatedUser: {
+          roles: {
+            admin: false,
+          }
+        }
+      },
+      body: {
+        roles: {
+          admin: true,
+        }
+      }
+    }
+
+    const next = (code) => {
+        expect(code).toBe(403);
+        done();
+      }
+
+    updateUser(req, {}, next);
+  })
+
+  it('Deberia de mostrar un error 400 si no envia email o password', (done) => {
+    const userId = users.insertedIds['0'];
+    const req = {
+      params: {
+        uid: userId,
+      },
+      headers: {
+        authenticatedUser: {
+          roles: {
+            admin: true,
+          }
+        }
+      },
+      body: {
+        roles: {
+          admin: true,
+        }
+      }
+    }
+
+    const next = (code) => {
+        expect(code).toBe(400);
+        done();
+      }
+
+    updateUser(req, {}, next);
+  })
+})
+
+describe('getUserById', () => {
+  beforeAll(async () => {
+    await db();
+    const collectionUsers = (await db()).collection('users');
+    users = await collectionUsers.insertMany([
+      {
+        email: 'user@test',
+        roles: { admin: true },
+      }
+    ]);
+  });
+
+  afterAll(async () => {
+    await db().close();
+  });
+
+  it('Deberia de poder obtener un usuario por su uid', () => {})
+  it('Deberia de poder obtener un usuario por su email', () => {})
+  it('Deberia de mostar un error 404 si no existe el usuario', () => {})
+})
+
+describe('getUsers', () => {
+  beforeAll(async () => {
+    await db();
+    const collectionUsers = (await db()).collection('users');
+    users = await collectionUsers.insertMany([
+      {
+        email: 'user@test',
+        roles: { admin: true },
+      },
+      {
+        email: 'user2@test',
+        roles: { admin: false },
+      },
+      {
+        email: 'user3@test',
+        roles: { admin: false },
+      }
+    ]);
+  });
+
+  afterAll(async () => {
+    await db().close();
+  });
+
+  it('Deberia de poder obtener un usuario por su uid', () => {})
+  it('Deberia de poder obtener un usuario por su email', () => {})
+  it('Deberia de mostar un error 404 si no existe el usuario', () => {})
 })
