0c1d46796747ef6c290253565c188f1b2c0eb95f
Danielalab <olabolacrayola@hotmail.com>
2019-11-12 15:49:01 -0500

    agrega unit tes getOrders

---
+++ controllers/__tests__/order.spec.js
@@ -0,0 +1,87 @@
+const {
+  getOrders
+} = require('../orders');
+
+const db = require('../../libs/connectdb');
+
+describe('getOrders', () => {
+  beforeAll(async () => {
+    await db();
+    await (await db()).collection('orders').insertMany([
+      {
+        userId: 'test123456',
+        client: 'Ana',
+        products: [
+          {
+            qty: 2,
+            product: {
+              name: 'Jugos de frutas natural',
+              price: 7,
+              image: 'http://jugo.img',
+              type: 'bebidas',
+              dateEntry: new Date(),
+            }
+          },
+          {
+            qty: 1,
+            product: {
+              name: 'Hamburguesa simple',
+              price: 10,
+              image: 'http://burger-simple.img',
+              type: 'hamburguesas',
+              dateEntry: new Date(),
+            }
+          }
+        ],
+        status: 'pending',
+        dateEntry: new Date(),
+      },
+      {
+        userId: 'testuserfake',
+        client: 'Ivan',
+        products: [
+          {
+            qty: 1,
+            product: {
+              name: 'Hamburguesa doble',
+              price: 15,
+              image: 'http://burger-double.img',
+              type: 'hamburguesas',
+              dateEntry: new Date(),
+            }
+          }
+        ],
+        status: 'delivered',
+        dateEntry: new Date(),
+        dateProcessed: new Date()
+      },
+    ])
+  })
+
+  afterAll(async() => {
+    await (await db()).collection('orders').deleteMany({});
+    await db().close();
+  })
+
+  it('Deberia de obtener 2 ordenes', () => {
+    const req = { query: {} };
+    const resp = {
+      send: (response) => {
+        expect(response.length).toBe(2);
+        expect(response[0].client).toBe('Ana');
+        expect(response[0].products.length).toBe(2);
+        expect(response[0].status).toBe('pending');
+        expect(response[1].client).toBe('Ivan');
+        expect(response[1].products.length).toBe(1);
+        expect(response[1].status).toBe('delivered');
+        done();
+      },
+      set: (nameHeader, header) => {
+        expect(nameHeader).toBe('link');
+        expect(header).toBe('</orders?limit=10&page=1>; rel="first", </orders?limit=10&page=1>; rel="prev", </orders?limit=10&page=1>; rel="next", </orders?limit=10&page=1>; rel="last"');
+        done();
+      }
+    }
+    getOrders(req, resp);
+  })
+})
