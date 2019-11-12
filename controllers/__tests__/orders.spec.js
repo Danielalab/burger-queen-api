473fe8c5911b9f962bdaadeeec9aa6396d2a982a
Danielalab <olabolacrayola@hotmail.com>
2019-11-12 16:13:44 -0500

    agrega unit tests getUserById

--- controllers/__tests__/orders.spec.js
+++ controllers/__tests__/orders.spec.js
@@ -1,5 +1,6 @@
 const {
-  getOrders
+  getOrders,
+  getOrderById
 } = require('../orders');
 
 const db = require('../../libs/connectdb');
@@ -83,5 +84,109 @@ describe('getOrders', () => {
       }
     }
     getOrders(req, resp);
+  })
+})
+
+describe('getOrderById', () => {
+  let orders;
+  beforeAll(async () => {
+    await db();
+    orders = await (await db()).collection('orders').insertMany([
+      {
+        userId: 'test123456',
+        client: 'Ana',
+        products: [
+          {
+            qty: 2,
+            product: {
+              name: 'Jugos de frutas natural',
+              price: 7,
+              image: 'http://jugo.img',
+              type: 'bebidas',
+              dateEntry: new Date(),
+            }
+          },
+          {
+            qty: 1,
+            product: {
+              name: 'Hamburguesa simple',
+              price: 10,
+              image: 'http://burger-simple.img',
+              type: 'hamburguesas',
+              dateEntry: new Date(),
+            }
+          }
+        ],
+        status: 'pending',
+        dateEntry: new Date(),
+      },
+      {
+        userId: 'testuserfake',
+        client: 'Ivan',
+        products: [
+          {
+            qty: 1,
+            product: {
+              name: 'Hamburguesa doble',
+              price: 15,
+              image: 'http://burger-double.img',
+              type: 'hamburguesas',
+              dateEntry: new Date(),
+            }
+          }
+        ],
+        status: 'delivered',
+        dateEntry: new Date(),
+        dateProcessed: new Date()
+      },
+    ])
+  })
+
+  afterAll(async() => {
+    await (await db()).collection('orders').deleteMany({});
+    await db().close();
+  })
+
+  it('Deberia de poder obtener una order por su id', (done) => {
+    const orderId = orders.insertedIds['0'];
+    const req = {
+      params: { orderId },
+    }
+    const resp = {
+      send: (response) => {
+        expect(response._id).toEqual(orderId)
+        expect(response.client).toBe('Ana');
+        expect(response.products.length).toBe(2);
+        expect(response.status).toBe('pending');
+        done();
+      }
+    }
+    getOrderById(req, resp);
+  })
+
+  it('Deberia obtener un error 404 si el usuario no existe', (done) => {
+    const req = {
+      params: {
+        orderId: '2dc33b22c2222222222222a'
+      },
+    }
+    const next = (code) => {
+      expect(code).toBe(404);
+      done();
+    };
+    getProductById(req, {}, next);
+  })
+
+  it('Deberia obtener un error 404 si el ID no es valido', (done) => {
+    const req = {
+      params: {
+        orderId: 'fakeorderid'
+      },
+    }
+    const next = (code) => {
+      expect(code).toBe(404);
+      done();
+    };
+    getProductById(req, {}, next);
   })
 })
